7ce9d24e549e3ba486ba13d1539f23dd851b2130
ismael747-wq <ismael747@gmail.com>
2026-07-23 01:50:36 -0300

    Update consultas_basicas.sql

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -1,1 +1,15 @@
+-- ══════════════════════════════════════════
+-- TechStore — Consultas Básicas SELECT
+-- Autor: [Tu nombre]
+-- Fecha: [Fecha de entrega]
+-- ══════════════════════════════════════════
+
+-- Para el primer caso usamos  SELECT * para traer todas las columnas de una tabla y dar un vistazo general a la misma
+select * from sales;
+
+-- Consulta 2: Selección de columnas específicas para finanzas
+select customer_id, product_id, total_amount from sales;
+
+-- Consulta 3: Selección con alias en español para stakeholders
+select order_date as fecha_pedido, product_name as nombre_producto, quantity as cantidad_unidades from sales;
 
